fb637e492c240165e9cb471fa2bc8ddd45868265
timm0e <11885527+timm0e@users.noreply.github.com>
2017-10-15 14:06:36 +0200

    Formatting and misc

--- index.ts
+++ index.ts
@@ -1,24 +1,24 @@
-import toml = require("tomljs");
 import fs = require("fs");
+import toml = require("tomljs");
 
-export class RaidBotConfig
-{
-    public clientID: string;
-    public clientSecret: string;
-    public guildID: string;
-    public hostname: string;
-    public filepath: string;
+export class RaidBotConfig {
+  public clientID: string;
+  public clientSecret: string;
+  public botSecret: string;
+  public guildID: string;
+  public hostname: string;
+  public soundpath: string;
 }
 
-function getPath(path?: string): string
-{
-    if (path)
-        return path;
-    else if (process.env.RAIDBOT_CFG)
-        return process.env.RAIDBOT_CFG as string;
-    return "/mnt/RaidBot/config.toml";
+function getPath(path?: string): string {
+  if (path) {
+    return path;
+  } else if (process.env.RAIDBOT_CFG) {
+    return process.env.RAIDBOT_CFG as string;
+  }
+  return "/mnt/RaidBot/config.toml";
 }
 
-module.exports = function(path?: string): RaidBotConfig {
-    return toml(fs.readFileSync(getPath(path)).toString()) as RaidBotConfig;
-}
+module.exports = (path?: string): RaidBotConfig => {
+  return toml(fs.readFileSync(getPath(path)).toString()) as RaidBotConfig;
+};
